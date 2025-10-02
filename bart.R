5dd8b085f37f88d34924318077e74cd75fcec054
Isaac Miller <imick5555@gmail.com>
2025-10-02 15:48:52 -0600

    changed bart
    bart is final submission for bike kaggle assignment

--- bart.R
+++ bart.R
@@ -8,11 +8,17 @@ library(glmnet)
 library(dials)
 library(rpart)
 library(ranger)
+library(bonsai)
+library(lightgbm)
 library(agua)
+library(dbarts)
+
 setwd("C://Users//Isaac//OneDrive//Documents//fall 2025 semester//STAT 348//BiekShare")
 
 train <- vroom("train.csv")
 test <- vroom("test.csv")
+
+# h2o::h2o.init()
 
 train_ud <- train |>
   select(-casual, -registered) |>
@@ -26,6 +32,7 @@ my_recipe <- recipe(count ~., data=train_ud) %>%
                               labels=c("Clear", "Mist", "Light Snow"))) %>%
   step_mutate(season = factor(season, levels=c("1","2","3","4"), 
                               labels=c("Spring","Summer","Fall","Winter"))) %>%
+  #step_time(datetime, features="decimal_day")%>%
   step_time(datetime, features="hour") %>%
   step_dummy(all_nominal_predictors()) %>%
   step_normalize(all_numeric_predictors())
@@ -33,56 +40,60 @@ my_recipe <- recipe(count ~., data=train_ud) %>%
 prepped_recipe <- prep(my_recipe) # Sets up the preprocessing using myDataSet13
 bake(prepped_recipe, new_data=train_ud)
 
-forest_mod <- rand_forest(mtry = tune(),
-                          min_n=tune(),
-                      trees = 500) %>% 
-  set_engine("ranger") %>% 
+bart_model <- parsnip::bart(trees=500) %>% # BART figures out depth and learn_rate
+  set_engine("dbarts") %>% # might need to install
   set_mode("regression")
 
-forest_workflow <- workflow() %>%
+# auto_model <- auto_ml() %>%
+# set_engine("h2o", max_runtime_secs=240, max_models=5) %>%
+# set_mode("regression")
+# 
+bart_workflow <- workflow() %>%
   add_recipe(my_recipe) %>%
-  add_model(forest_mod)
+  add_model(bart_model) %>%
+  fit(data=train_ud)
 
 # 
-forest_grid <- grid_regular(mtry(range=c(1, (ncol(bake(prepped_recipe, new_data = NULL) - 1)))),
-                     min_n(),
-                     levels=5)
+bart_grid <- grid_regular(trees(), levels = 5)
 # 
 folds <- vfold_cv(train_ud, v = 5, repeats=1)
-# 
-CV_results <- forest_workflow %>%
+#
+CV_results <- bart_workflow %>%
   tune_grid(resamples=folds,
-            grid=forest_grid,
+            grid=bart_grid,
             metrics=metric_set(rmse, mae))
-# 
+# # 
 collect_metrics(CV_results) %>% # Gathers metrics into DF
   filter(.metric=="rmse")
 # %>%
 #   ggplot(data=., aes(x=penalty, y=mean, color=factor(mixture))) +
 #   geom_line()
 # 
-bestTune <- CV_results %>%
-  select_best(metric="rmse")
+# bestTune <- CV_results %>%
+#   select_best(metric="rmse")
 
-final_wf <-
-  forest_workflow %>%
-  finalize_workflow(bestTune) %>%
-  fit(data=train_ud)
-
-final_wf %>%
+# final_wf <-
+#   bart_workflow %>%
+#   finalize_workflow(bestTune) %>%
+#   fit(data=train_ud)
+# 
+bart_workflow %>%
   predict(new_data = test)
+
+# final_wf %>%
+#   predict(new_data = test)
 
 
 
 ## Run all the steps on test data15
-forest_predictions <- predict(final_wf, new_data = test)
+bart_predictions <- predict(bart_workflow, new_data = test)
 
 
 
-forest_predictions
+bart_predictions
 
 
-kaggle_submission <- forest_predictions %>%
+kaggle_submission <- bart_predictions %>%
   bind_cols(., test) %>%
   select(datetime, .pred) %>%
   rename(count=.pred) %>%
@@ -91,3 +102,4 @@ kaggle_submission <- forest_predictions %>%
   mutate(datetime=as.character(format(datetime)))
 
 vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
+
